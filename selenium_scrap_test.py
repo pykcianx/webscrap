2a8b842ad64a407030555a398d016ccfde7f87ff
pykcianx <sznapi221@wp.pl>
2021-11-21 12:54:59 +0100

    commit2

--- selenium_scrap_test.py
+++ selenium_scrap_test.py
@@ -1,4 +1,3 @@
-from os import replace
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -12,11 +11,11 @@ options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH, options=options)
-driver.get("exampel.com")
+driver.get("https://panoramafirm.pl/tłumaczenia")
 
 
 driver.find_element_by_xpath('/html/body/div[6]/div[1]/div/div/div[4]/i').click()
-driver.find_element_by_xpath('/html/body/div[6]/div[2]/div/div[2]/button').click()
+driver.find_element_by_xpath('/html/body/div[6]/div[2]/div/div[1]/div[2]/div[2]/button').click()
 
 
 def extractfirmy():
@@ -26,20 +25,18 @@ def extractfirmy():
 
     listalist = zip(nazwy, numerytel, maile)
     for nazwa, numer, mail in listalist:
-        n = nazwa.get_attribute("text")
+        n = nazwa.get_attribute("text").strip()
         nr = numer.get_attribute("data-original-title")
         m = mail.get_attribute("data-company-email")
 
         print(n,nr,m)
+        #df = pd.read
         writer.writerow([n,nr,m])
 
-        #df14['tup'] = df14.apply(lambda x: list(zip(x.key,x.hi)), axis=1)
-        #print (df14)
 
 
-with open('pfkam.csv', 'w', newline='', encoding='utf_8_sig') as csvfile:
-    writer = csv.writer(csvfile, dialect = 'excel', delimiter=",")
-
+with open('pftlumaczenia.csv', 'w', newline='', encoding='utf_8_sig') as csvfile:
+    writer = csv.writer(csvfile, dialect = 'excel', delimiter="|")
 
     for strona in range(5): 
         naststrona = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "i[class='icon-arrow-right']")))
